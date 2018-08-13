1d8d22bdbbf8ac27682b83dd511f1b8f42c6118d
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2018-08-13 20:07:01 +0530

    Update Info_Queries.sql

--- Must Know/Info_Queries.sql
+++ Must Know/Info_Queries.sql
@@ -1,2 +1,5 @@
 # Gives info about table - columns, data types, created date, etc.
 exec sp_help tableName
+
+#Best resource
+#https://dataedo.com/blog/useful-sql-server-data-dictionary-queries-every-dba-should-have#toc_1
