dce98043d2ccb763e3aafa3cd597c2f36882d6d0
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2018-08-14 13:38:45 +0530

    Update Table columns details.sql

--- Information Queries/Table columns details.sql
+++ Information Queries/Table columns details.sql
@@ -1,4 +1,5 @@
 # This query returns list of tables and their columns with details.
+# Resource: https://dataedo.com/blog/useful-sql-server-data-dictionary-queries-every-dba-should-have#toc_1
 # https://dataedo.com/asset/img/blog/sql_server_data_dictionary_tables_columns_query_results.png
 
 select schema_name(tab.schema_id) as schema_name,
