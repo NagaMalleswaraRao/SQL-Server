e82f8ad66341ae9afd19c7ca493355abe5f0d11d
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2018-08-14 13:37:39 +0530

    Update List of Views with columns.sql

--- Information Queries/List of Views with columns.sql
+++ Information Queries/List of Views with columns.sql
@@ -1,4 +1,5 @@
 # This query returns list of views with their columns.
+# Resource: https://dataedo.com/blog/useful-sql-server-data-dictionary-queries-every-dba-should-have#toc_1
 # https://dataedo.com/asset/img/blog/sql_server_data_dictionary_views_columns_query_results.png
 
 select schema_name(v.schema_id) as schema_name,
