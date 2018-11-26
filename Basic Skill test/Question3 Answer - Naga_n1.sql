fe3afaa81dad0b4b170b9260118c24d5d2a7aaf8
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2018-11-26 17:50:53 +0530

    Update Question3 Answer - Naga_n1.sql

--- Basic Skill test/Question3 Answer - Naga_n1.sql
+++ Basic Skill test/Question3 Answer - Naga_n1.sql
@@ -2,7 +2,6 @@
 with Scores as 
 (select ROW_NUMBER() over(order by Score desc) as 'row',* 
  from [NagaDB].[dbo].[Employee])
-
 select * from Scores
 where row=3
 
@@ -10,6 +9,21 @@ where row=3
 with Scores as 
 (select Dense_rank() over(order by Score desc) as 'row',* 
  from [NagaDB].[dbo].[Employee])
-
 select * from Scores
 where row=3
+
+-- Primitive method                      
+select Top 1 * from
+[NagaDB].[dbo].[Item_Price]
+where [Selling_Price]< (select max([Selling_Price]) from
+					[NagaDB].[dbo].[Item_Price])
+order by Selling_Price desc
+
+--Using Offset and Fetch Next                     
+select Item, [Selling_Price] 
+from [NagaDB].[dbo].[Item_Price]
+order by  Selling_Price desc, Item desc
+offset 1 rows
+fetch next 1 rows only
+                     
+                     
