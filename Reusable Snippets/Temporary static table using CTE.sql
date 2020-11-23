018500b21e4f1ff48e21b7f93feffb06f035e161
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2020-11-23 15:54:11 +0530

    Update Temporary static table using CTE.sql

--- Reusable Snippets/Temporary static table using CTE.sql
+++ Reusable Snippets/Temporary static table using CTE.sql
@@ -7,3 +7,17 @@ Select 'US21', 'RDC', 'RDC', 'Denver'
 )
 
 Select * from Plant_map
+
+------------------------------------------------
+
+With dataz (id,[subject],[from],[to],[timestamp]) as
+(
+select '1', 'Yosemite','zach@g.com','thomas@g.com', cast('2018-01-02 12:45:03' as datetime) Union
+select '2', 'Big Sur','sarah@g.com','thomas@g.com', cast('2018-01-02 16:30:01' as datetime) Union
+select '3', 'Yosemite','thomas@g.com','zach@g.com', cast('2018-01-02 16:35:04' as datetime) Union
+select '4', 'Running','jill@g.com','zach@g.com', cast('2018-01-03 08:12:45' as datetime) Union
+select '5', 'Yosemite','zach@g.com','thomas@g.com', cast('2018-01-03 14:02:01' as datetime) Union
+select '6', 'Yosemite','thomas@g.com','zach@g.com', cast('2018-01-03 15:01:05' as datetime) 
+)
+
+Select * from dataz
