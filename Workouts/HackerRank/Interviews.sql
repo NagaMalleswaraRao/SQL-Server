58798bb56339fea088650bbccf2bb636e7831e37
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2021-01-05 16:31:05 +0530

    Update Interviews.sql

--- Workouts/HackerRank/Interviews.sql
+++ Workouts/HackerRank/Interviews.sql
@@ -1,4 +1,5 @@
---contest_id, hacker_id, name, and the sums of total_submissions, total_accepted_submissions, total_views, and total_unique_views for each contest sorted by contest_id. Exclude the contest from the result if all four sums are 0.
+--contest_id, hacker_id, name, and the sums of total_submissions, total_accepted_submissions, total_views, and total_unique_views for each contest sorted by contest_id. 
+--Exclude the contest from the result if all four sums are 0.
 --Colleges, Challenges, View_Stats, Submission_Stats
 With CTE as
 (
